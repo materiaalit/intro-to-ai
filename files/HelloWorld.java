887541f01a3453eb955c629ee734a5cde71e8987
hnbot <nygrenh+hnbot@gmail.com>
2018-09-03 04:54:53 +0000

    Update: Add api key

---
+++ files/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.lejos.example;
+
+import lejos.nxt.*;
+import lejos.robotics.navigation.DifferentialPilot;
+
+/**
+ * Write your code in main()
+ */
+public class HelloWorld {
+    static DifferentialPilot pilot = new DifferentialPilot(5.6, 17.5f, Motor.A, Motor.B);
+    static UltrasonicSensor ultra = new UltrasonicSensor(SensorPort.S2);
+    static LightSensor light = new LightSensor(SensorPort.S1);
+
+    public static void main(String[] args) {
+        System.out.println("I'll be back!");
+        Button.waitForAnyPress();
+    }
+}
